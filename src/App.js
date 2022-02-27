78e179e3c9df74df37f82e4f19a662e2bb86b192
yhupaul <yhupaul@hotmail.com>
2022-02-27 15:02:56 -0800

    practice react with todolist

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) =>  setToDo(event.target.value);
+  const onSumit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      return;
+    };
+    setToDos((currentArr) => [toDo, ...currentArr]);
+    setToDo("");
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSumit}>
+        <input 
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
